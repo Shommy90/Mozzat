b7fea3acb3a26c86fda009d9e09b0e4e294e3789
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-07-01 17:17:09 +0200

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Router from "./Router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router />
+      <Footer />
     </div>
   );
 }
