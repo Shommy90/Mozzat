a584c40b85269de1af7d0d8e2900ce69d21fcb3e
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-07-03 15:16:04 +0200

    add welcome section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import homeBg from "../../assets/images/header-bg.png";
+import homeLogo from "../../assets/images/logo-header.png";
+import mozzatHome from "../../assets/images/Mozzat.png";
 
 const Home = () => {
-  return <div className="home content">Home</div>;
+  return (
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${homeBg})`,
+        backgroundSize: "auto",
+        backgroundPosition: "center"
+      }}
+    >
+      <div
+        className="content flex align-center justify-center"
+        style={{ background: "rgba(0,0,0, .7)", minHeight: "100vh" }}
+      >
+        <div>
+          <div style={{ width: "205px", height: "234px", margin: "0 auto" }}>
+            <img src={homeLogo} />
+          </div>
+
+          <div style={{ maxWidth: "581px", width: "100%", height: "230px" }}>
+            <img src={mozzatHome} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
