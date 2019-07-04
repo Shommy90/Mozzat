f216b15da1d4c4ea97661f29d62409dff25fe804
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-07-04 14:59:57 +0200

    start products section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,32 +2,36 @@ import React from "react";
 import homeBg from "../../assets/images/header-bg.png";
 import homeLogo from "../../assets/images/logo-header.png";
 import mozzatHome from "../../assets/images/Mozzat.png";
+import ProductsSection from "../sections/ProductsSection";
 
 const Home = () => {
   return (
-    <div
-      className="home"
-      style={{
-        backgroundImage: `url(${homeBg})`,
-        backgroundSize: "auto",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat"
-      }}
-    >
+    <div className="home">
       <div
-        className="content flex align-center justify-center"
-        style={{ background: "rgba(0,0,0, .7)", minHeight: "100vh" }}
+        className="home-section"
+        style={{
+          backgroundImage: `url(${homeBg})`,
+          backgroundSize: "auto",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat"
+        }}
       >
-        <div>
-          <div style={{ width: "205px", height: "234px", margin: "0 auto" }}>
-            <img src={homeLogo} alt="home logo" />
-          </div>
+        <div
+          className="content flex align-center justify-center"
+          style={{ background: "rgba(0,0,0, .7)", minHeight: "100vh" }}
+        >
+          <div>
+            <div style={{ width: "205px", height: "234px", margin: "0 auto" }}>
+              <img src={homeLogo} alt="home logo" />
+            </div>
 
-          <div style={{ maxWidth: "581px", width: "100%", height: "230px" }}>
-            <img src={mozzatHome} alt="welcome" />
+            <div style={{ maxWidth: "581px", width: "100%", height: "230px" }}>
+              <img src={mozzatHome} alt="welcome" />
+            </div>
           </div>
         </div>
       </div>
+      <ProductsSection />
     </div>
   );
 };
---
+++ src/pages/sections/ProductsSection/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import productsSectionBg from "../../../assets/images/products-section-bg.png";
+
+const ProductsSection = () => {
+  return (
+    <div
+      className="products-section"
+      style={
+        {
+          // backgroundImage: `url(${productsSectionBg})`,
+          // backgroundSize: "cover",
+          // backgroundPosition: "center",
+          // backgroundRepeat: "no-repeat",
+          // height: "1200px"
+        }
+      }
+    >
+      products section
+    </div>
+  );
+};
+
+export default ProductsSection;
