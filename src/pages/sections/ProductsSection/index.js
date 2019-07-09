5df701e69d80f6ae36b56f7f3111d4f2e344da96
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-07-09 23:34:29 +0200

    add products section bg

--- src/pages/sections/ProductsSection/index.js
+++ src/pages/sections/ProductsSection/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import productsSectionBg from "../../../assets/images/products-section-bg.png";
+import productsTitle from "../../../assets/images/products-title.png";
 
 const ProductsSection = () => {
   return (
     <div
       className="products-section"
-      style={
-        {
-          // backgroundImage: `url(${productsSectionBg})`,
-          // backgroundSize: "cover",
-          // backgroundPosition: "center",
-          // backgroundRepeat: "no-repeat",
-          // height: "1200px"
-        }
-      }
+      style={{
+        backgroundImage: `url(${productsSectionBg})`,
+        backgroundSize: "auto",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "1200px"
+      }}
     >
-      products section
+      <div className="products-carousel">
+        <div className="container">
+          <div className="flex justify-center" style={{ marginBottom: "50px" }}>
+            <img
+              src={productsTitle}
+              style={{ width: "311px", height: "82px" }}
+            />
+          </div>
+
+          <div className="carousel">CAROUSEL</div>
+        </div>
+      </div>
     </div>
   );
 };
