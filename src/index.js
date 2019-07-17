3a7e2de37e638631d536b4a63893bbbe9f600a45
Shommy <marjanovic90@gmail.com>
2019-07-17 13:53:59 +0200

    gh-pages router fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./assets/styles/reset.scss";
 import "./assets/styles/main.scss";
 import App from "./App";
 
 const app = (
-  <BrowserRouter basename="/Mozzat">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
