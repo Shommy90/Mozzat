4b0fbf61a5d0ea08ecbefa89a63d515d6ada5a31
Shommy <marjanovic90@gmail.com>
2019-07-17 13:48:48 +0200

    Updates

--- precache-manifest.1cea1f00ad4b3393823ac75879f8a3ca.js
+++ precache-manifest.1cea1f00ad4b3393823ac75879f8a3ca.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7fc0cc529cf2cf32fdcb56ed1d442a58",
+    "revision": "a390264d642de180d361f9c1b7ea91e4",
     "url": "/Mozzat/index.html"
   },
   {
-    "revision": "62fae31743a2b3c9f88e",
+    "revision": "1c1d318ec618876ccfc8",
     "url": "/Mozzat/static/css/2.1fd2b905.chunk.css"
   },
   {
-    "revision": "b1d09b643e04bb39fd80",
+    "revision": "3896f314c9a100a61a95",
     "url": "/Mozzat/static/css/main.022e1bc7.chunk.css"
   },
   {
-    "revision": "62fae31743a2b3c9f88e",
-    "url": "/Mozzat/static/js/2.2409ce00.chunk.js"
+    "revision": "1c1d318ec618876ccfc8",
+    "url": "/Mozzat/static/js/2.d01dbe48.chunk.js"
   },
   {
-    "revision": "b1d09b643e04bb39fd80",
-    "url": "/Mozzat/static/js/main.f98d6e06.chunk.js"
+    "revision": "3896f314c9a100a61a95",
+    "url": "/Mozzat/static/js/main.247979c3.chunk.js"
   },
   {
     "revision": "605918599d0c654c9b3e",
