2662c4f3cf42abde1e5642d63566b1abe0c3174a
Shommy <marjanovic90@gmail.com>
2019-07-17 13:16:42 +0200

    add Contact and Gallery pages

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import GoogleMap from "../../components/GoogleMap";
+import ContactForm from "../../components/ContactForm";
+import Socials from "../../components/Socials";
 
 import contactBg from "../../assets/images/contact-bg.png";
 import contactTitle from "../../assets/images/contact-title.png";
+import anyQuestions from "../../assets/images/any-questions.png";
+import contactMapBg from "../../assets/images/contact-map-bg.png";
 
 const Contact = () => {
   return (
@@ -22,8 +26,40 @@ const Contact = () => {
 
       <GoogleMap />
 
-      <div style={{ padding: "90px 0 130px" }}>
-        <div className="contact-wrapper">contact</div>
+      <div style={{ padding: "70px 0 100px" }}>
+        <div
+          style={{
+            width: "270px",
+            height: "58px",
+            margin: "0 auto",
+            marginBottom: "-15px"
+          }}
+        >
+          <img src={anyQuestions} alt="any questions" />
+        </div>
+        <h3
+          style={{
+            fontSize: "36px",
+            fontWeight: "500",
+            textAlign: "center",
+            marginBottom: "50px"
+          }}
+        >
+          BE FREE AND CONTACT US
+        </h3>
+
+        <div
+          className="contact-holder-bg"
+          style={{ backgroundImage: `url(${contactMapBg})` }}
+        >
+          <div className="flex flex-col align-center justify-center">
+            <div className="contact-socials">
+              <Socials />
+            </div>
+
+            <ContactForm />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,7 +1,77 @@
 import React from "react";
 
+import galleryBg from "../../assets/images/gallery-bg.png";
+import galleryTitle from "../../assets/images/gallery-title.png";
+
+import BeefBurger from "../../assets/images/gallery/beef-burger.jpg";
+import BeefJucyLucy from "../../assets/images/gallery/beef-jucy-lucy.jpg";
+import BeefBurger2 from "../../assets/images/gallery/beef-burger-2.jpg";
+import KababLamb from "../../assets/images/gallery/kabab-lamb.jpg";
+import KababChicken from "../../assets/images/gallery/kabab-chicken.jpg";
+import ChickenWings from "../../assets/images/gallery/chicken-wings.jpg";
+import Koftah from "../../assets/images/gallery/koftah.jpg";
+import LambShank from "../../assets/images/gallery/lamb-shank.jpg";
+import TBoneBeef from "../../assets/images/gallery/t-bone-beef.jpg";
+import WholeChickenNormal from "../../assets/images/gallery/whole-chicken-normal.jpg";
+import LambRack from "../../assets/images/gallery/lamb-rack.jpg";
+import ShortLoinLamb from "../../assets/images/gallery/Short-loin-lamb.jpg";
+
+const images = [
+  { img: BeefBurger, name: "Beef Burger" },
+  { img: BeefJucyLucy, name: "Beef Jucy Lucy" },
+  { img: BeefBurger2, name: "Beef Burger 2" },
+  { img: KababLamb, name: "Kabab Lamb" },
+  { img: KababChicken, name: "Kabab Chicken" },
+  { img: ChickenWings, name: "Chicken Wings" },
+  { img: Koftah, name: "Koftah" },
+  { img: LambShank, name: "Lamb Shank" },
+  { img: TBoneBeef, name: "T-Bone Beef" },
+  { img: WholeChickenNormal, name: "Whole Chicken Normal" },
+  { img: LambRack, name: "Lamb Rack" },
+  { img: ShortLoinLamb, name: "Short Loin Lamb" }
+];
+
 const Gallery = () => {
-  return <div className="gallery content">gallery</div>;
+  return (
+    <div className="gallery content">
+      <div
+        className="page-header"
+        style={{ backgroundImage: `url(${galleryBg})` }}
+      >
+        <div className="dark-bg">
+          <div
+            className="header-title-holder"
+            style={{ paddingBottom: "70px" }}
+          >
+            <div style={{ display: "inline-block" }}>
+              <img src={galleryTitle} alt="title" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ padding: "100px 0" }}>
+        <div className="gallery-wrapper">
+          <div className="list-holder" style={{ marginBottom: "50px" }}>
+            <ul className="gallery-list">
+              {images.map((img, index) => (
+                <li key={index} style={{ backgroundImage: `url(${img.img})` }}>
+                  <div>{img.name}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <button
+            className="button"
+            style={{ margin: "0 auto", display: "none" }}
+          >
+            load more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
