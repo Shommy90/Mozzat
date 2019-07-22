336cfddfe3dd6e39524cdd5d932f2559dff48dea
Shommy <marjanovic90@gmail.com>
2019-07-22 16:02:19 +0200

    set contact form inputs

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,31 +1,71 @@
 import React from "react";
 
-const ContactForm = () => {
-  return (
-    <div
-      className="contact-form"
-      style={{
-        maxWidth: "340px",
-        width: "100%",
-        padding: "0 10px",
-        margin: "0 auto"
-      }}
-    >
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message" />
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
 
-        <button
-          className="button"
-          type="submit"
-          style={{ margin: "0 auto", marginTop: "25px" }}
-        >
-          send
-        </button>
-      </form>
-    </div>
-  );
-};
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div
+        className="contact-form"
+        style={{
+          maxWidth: "340px",
+          width: "100%",
+          padding: "0 10px",
+          margin: "0 auto"
+        }}
+      >
+        <form>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+          />
+          <input
+            onChange={this.handleChange}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+          <textarea
+            onChange={this.handleChange}
+            id="message"
+            name="message"
+            placeholder="Message"
+          />
+
+          <button
+            className="button"
+            type="submit"
+            style={{ margin: "0 auto", marginTop: "25px" }}
+            onClick={e => this.handleFormSubmit(e)}
+          >
+            send
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactForm;
