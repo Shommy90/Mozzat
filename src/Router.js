be4266a624fe36ed79396654f4d4e07b651f074e
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-07-23 22:13:32 +0200

    fix router scroll restoration

--- src/Router.js
+++ src/Router.js
@@ -8,17 +8,20 @@ import Products from "./pages/Products/index";
 import ProductItem from "./pages/ProductItem/index";
 import Contact from "./pages/Contact/index";
 import B2B from "./pages/B2B/index";
+import ScrollToTop from "./components/ScrollToTop";
 
 const Router = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/gallery" component={Gallery} />
-    <Route path="/products" component={Products} />
-    <Route path="/item" component={ProductItem} />
-    <Route path="/contact" component={Contact} />
-    <Route path="/b2b" component={B2B} />
-  </Switch>
+  <ScrollToTop>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/products" component={Products} />
+      <Route path="/item" component={ProductItem} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/b2b" component={B2B} />
+    </Switch>
+  </ScrollToTop>
 );
 
 export default Router;
