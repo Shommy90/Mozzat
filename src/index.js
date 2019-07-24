9b112deb0ab55de0eae395c260343b88beaca589
Shommy <marjanovic90@gmail.com>
2019-07-24 12:38:20 +0200

    add react-router-scroll-memory

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactModal from "react-modal";
 
 import "./assets/styles/reset.scss";
 import "./assets/styles/main.scss";
 import App from "./App";
+import ScrollMemory from "react-router-scroll-memory";
 
 ReactModal.setAppElement("#root");
 
 const app = (
   <HashRouter>
+    <ScrollMemory />
     <App />
   </HashRouter>
 );
