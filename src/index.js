0083d7f2c6bf9ab8fbacb6e940fa217fc8aaf0d0
Shommy <marjanovic90@gmail.com>
2019-09-18 13:47:34 +0200

    add HashRouter & app tab title name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { HashRouter } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import ReactModal from "react-modal";
 
 import "./assets/styles/reset.scss";
@@ -12,10 +12,10 @@ import ScrollMemory from "react-router-scroll-memory";
 ReactModal.setAppElement("#root");
 
 const app = (
-  <BrowserRouter>
+  <HashRouter>
     <ScrollMemory />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
